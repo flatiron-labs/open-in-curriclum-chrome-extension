53cb63b87fbf8f817cdb1d62bd1d17d66f70845c
Amanda Chang <amanda.chang10@gmail.com>
2015-04-17 14:25:03 -0400

    4th digit optional

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
     repo = url.split("https://github.com/flatiron-school-ironboard/")[1];
-    var pattern = /^(.+)-.+-\d\d\d$/
+    var pattern = /^(.+)-.+-\d\d\d\d?$/
     real_repo = pattern.exec(repo);
     chrome.tabs.create({ url: "https://github.com/flatiron-school-curriculum/"+real_repo[1] });
   });
