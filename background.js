0df80e770cbbc0587cf94089446a2f3486128248
Avi Flombaum <avi@flombaum.com>
2015-05-05 21:19:37 -0400

    Update to learn-co organizations.

--- background.js
+++ background.js
@@ -2,9 +2,9 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
 {
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
-    repo = url.split("https://github.com/flatiron-school-ironboard/")[1];
+    repo = url.split("https://github.com/learn-co-students/")[1];
     var pattern = /^(.+)-.+-\d\d\d$/
     real_repo = pattern.exec(repo);
-    chrome.tabs.create({ url: "https://github.com/flatiron-school-curriculum/"+real_repo[1] });
+    chrome.tabs.create({ url: "https://github.com/learn-co-curriculum/"+real_repo[1] });
   });
 });
