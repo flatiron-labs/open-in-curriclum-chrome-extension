ebf7b8af39b00597535390a3ca9ee13d0da0d53d
Avi Flombaum <avi@flombaum.com>
2015-05-29 12:42:52 -0400

    Update background.js

--- background.js
+++ background.js
@@ -3,8 +3,13 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
     repo = url.split("https://github.com/learn-co-students/")[1];
-    var pattern = /^(.+)-.+-\d\d\d$/
-    real_repo = pattern.exec(repo);
-    chrome.tabs.create({ url: "https://github.com/learn-co-curriculum/"+real_repo[1] });
+    if (repo.match("000")){
+      var real_repo = repo.replace("-000", "")
+      chrome.tabs.create({ url: "https://github.com/learn-co-curriculum/"+real_repo });  
+    } else {
+      var pattern = /^(.+)-.+-\d\d\d\d?$/
+      var real_repo = pattern.exec(repo);
+      chrome.tabs.create({ url: "https://github.com/learn-co-curriculum/"+real_repo[1] });  
+    }
   });
 });
